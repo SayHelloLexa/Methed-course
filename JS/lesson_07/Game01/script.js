42ab90e524cfd8191674bd675644cb07c239fffc
SayHelloLexa <kds539489@gmail.com>
2024-10-25 16:24:02 +0300

    added lesson_07

--- JS/lesson_07/Game01/script.js
+++ JS/lesson_07/Game01/script.js
@@ -1,16 +1,38 @@
 'use strict';
 
-const hiddenNumber = Math.floor(Math.random() * 100 + 1);
-let answer = +prompt('Угадай число от 1 до 100');
-
-while (answer !== hiddenNumber) {
-  if (answer > hiddenNumber) {
-    alert('Меньше!');
-    answer = +prompt('Угадай число от 1 до 100', [1]);
-  } else if (answer < hiddenNumber) {
-    alert('Больше!');
-    answer = +prompt('Угадай число от 1 до 100', [1]);
+let typeOfAnswer = () => {
+  while (Number.isNaN(+answer)) {
+    alert('Введи число!');
+    answer = prompt('Угадай число от 1 до 100');
   }
 }
 
-alert('Правильно');
+const hiddenNumber = Math.floor(Math.random() * 100) + 1;
+let answer = prompt('Угадай число от 1 до 100');
+
+if (answer === null) {
+  alert('Игра окончена.');
+} else {
+  typeOfAnswer();
+  
+  while (+answer !== hiddenNumber) {
+    if (answer === null) {
+      alert('Игра окончена.');
+      break;
+    }
+  
+    if (+answer > hiddenNumber) {
+      alert('Меньше!');
+      answer = prompt('Угадай число от 1 до 100');
+      typeOfAnswer();
+    } else if (+answer < hiddenNumber) {
+      alert('Больше!');
+      answer = prompt('Угадай число от 1 до 100');
+      typeOfAnswer();
+    }
+  }
+
+  if (+answer === hiddenNumber) {
+    alert('Правильно!'); 
+  }
+}
