407c651273481c40235cc359513d5a790297b252
SayHelloLexa <kds539489@gmail.com>
2024-10-25 20:01:26 +0300

    added lesson_07

--- JS/lesson_07/Game02/script.js
+++ JS/lesson_07/Game02/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
-let typeOfAnswer = () => {
+let typeOfAnswer = (answer) => {
   while (Number.isNaN(+answer)) {
     alert('Введи число!');
     answer = prompt(`Угадай число от ${(+n > +m) ? +m : +n} до ${(+n > +m) ? +n : +m}`);
+  }
+
+  return answer;
+}
+
+let typeOfFirstRangeNumber = () => {
+  while (Number.isNaN(+n)) {
+    alert('Введи число!');
+    n = prompt('Введите первое число диапазона:');
+  }
+}
+
+let typeOfSecondRangeNumber = () => {
+  while (Number.isNaN(+m)) {
+    alert('Введи число!');
+    m = prompt('Введите первое число диапазона:');
   }
 }
 
@@ -21,43 +37,73 @@ let setHiddenNumber = (n, m) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const n = prompt('Введите первое число диапазона:');
-const m = prompt('Введите второе число диапазона:');
+let countAttempt = () => {
+  const min = (+n > +m) ? +m : +n;
+  const max = (+n > +m) ? +n : +m;
+
+  if (min === 50 && max === 100) {
+    return 15;
+  }
+
+  return (max - min + 1) * 0.3;
+}
+
+let n = prompt('Введите первое число диапазона:');
+
+typeOfFirstRangeNumber();
+
+let m = prompt('Введите второе число диапазона:');
+
+typeOfSecondRangeNumber();
+
 const hiddenNumber = setHiddenNumber(n, m);
-let answer = prompt(`Угадай число от ${(+n > +m) ? +m : +n} до ${(+n > +m) ? +n : +m}`);
-const rememberedNumbers = [];
 
-typeOfAnswer();
+let attempt = countAttempt();
+console.log(attempt);
 
-rememberedNumbers.push(+answer);
+if (isRangeNumberNull()) {
+  alert('Игра окончена.')
+} else {
+  let answer;
+  const rememberedNumbers = [];
 
-console.log(rememberedNumbers);
+  while (+answer !== hiddenNumber) {
+    console.log(rememberedNumbers);
+    console.log(attempt);
 
+    answer = prompt(`Угадай число от ${(+n > +m) ? +m : +n} до ${(+n > +m) ? +n : +m}`);
+    answer = typeOfAnswer(answer);
 
+    if (answer === null) {
+      alert('Игра окончена.');
+      break;
+    }
 
-// if (answer === null) {
-//   alert('Игра окончена.');
-// } else {
-//   typeOfAnswer();
-  
-//   while (+answer !== hiddenNumber) {
-//     if (answer === null) {
-//       alert('Игра окончена.');
-//       break;
-//     }
-  
-//     if (+answer > hiddenNumber) {
-//       alert('Меньше!');
-//       answer = prompt('Угадай число от 1 до 100');
-//       typeOfAnswer();
-//     } else if (+answer < hiddenNumber) {
-//       alert('Больше!');
-//       answer = prompt('Угадай число от 1 до 100');
-//       typeOfAnswer();
-//     }
-//   }
+    if (rememberedNumbers.includes(+answer) && !Number.isNaN(+answer)) {
+      alert('Это число вы уже вводили');
+      continue;
+    }
 
-//   if (+answer === hiddenNumber) {
-//     alert('Правильно!'); 
-//   }
-// }
+    if (!Number.isNaN(+answer)) {
+      rememberedNumbers.push(+answer);
+    }
+
+    if (+answer > hiddenNumber) {
+      alert('Меньше!');
+      attempt--;
+    } else if (+answer < hiddenNumber) {
+      alert('Больше!');
+      attempt--;
+    }
+
+    if (attempt === 0) {
+      alert('Игра окончена.');
+      break;
+    }
+
+    if (+answer === hiddenNumber) {
+      alert('Правильно!'); 
+      break;
+    }
+  }
+}
