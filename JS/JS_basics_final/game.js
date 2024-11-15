fd21138271f6c39c0303773ad8f1e0a11e175b23
SayHelloLexa <kds539489@gmail.com>
2024-11-15 22:18:53 +0300

    upd game

--- JS/JS_basics_final/game.js
+++ JS/JS_basics_final/game.js
@@ -1,38 +1,51 @@
 'use strict';
 
+// Вся логика игры делится на два этапа: игра в RPS и игра в марблы
+
 (() => {
   const game = () => {
+    // глобальные переменные
     let userCountBalls = 5;
     let computerCountBalls = 5;
     let continueGame = true;
 
+    // Генрация случайного числа для компьютера в RPS
     const getRandomIntInclusive = (min, max) => {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
+
+    // Ход компьютера в RPS
     const getComputerMove = () => {
       const moves = ['камень', 'ножницы', 'бумага'];
       return moves[getRandomIntInclusive(0, 2)];
     };
 
+    // Проверка правильности ввода игрока в RPS
+    // Принимает массив validMoves
+    // коллбек функция
+    // возвращает элемент, если коллбек вернет true
     const normalizeMove = (move) => {
-      const moveMap = {
-        'кам': 'камень',
-        'к': 'камень',
-        'нож': 'ножницы',
-        'н': 'ножницы',
-        'бум': 'бумага',
-        'б': 'бумага'
-      };
-      return moveMap[move] || move;
+      const validMoves = ['камень', 'ножницы', 'бумага'];
+      return validMoves.find(validMove => validMove.toLowerCase().startsWith(move.toLowerCase()));
     };
 
-    const determineWinner = (userMove, computerMove) => {
-      userMove = normalizeMove(userMove);
-      computerMove = normalizeMove(computerMove);
+    // Проверяет правильность ввода игрока уже в марблах
+    // когда компьютер делает ход, задаванием вопроса
+    const normalizeParity = (parity) => {
+      const validParities = ['чёт', 'нечёт'];
+      return validParities.find(validParity => validParity.toLowerCase().startsWith(parity.toLowerCase()));
+    };
 
+    // Функция определения победителя в RPS
+    // принимает ход игрока и ход компьютера
+    // возвращает ничью, если ходы одинаковые
+    // в противном случае обрабатывает исход, 
+    // когда ходы разные 
+    // и возвращает победителя
+    const determineWinner = (userMove, computerMove) => {
       if (userMove === computerMove) {
         return 'ничья';
       } else if (
@@ -46,6 +59,7 @@
       }
     };
 
+    // Функция игры в RPS
     const playRPS = () => {
       const userMove = prompt('Выберите: камень, ножницы или бумага');
       if (userMove === null) {
@@ -57,35 +71,56 @@
         }
       }
 
-      const validMoves = ['камень', 'ножницы', 'бумага', 'кам', 'нож', 'бум', 'к', 'н', 'б'];
-      if (!validMoves.includes(userMove)) {
+      // метод trim() убирает пробелы в начале и в конце строки
+      // возвращает новую строку без пробелов
+      if (userMove.trim() === '') {
         alert('Выберите: камень, ножницы или бумага!');
         return playRPS();
       }
 
+      // массив корректных ходов в RPS
+      // в качестве значения normalizedMove хранит элемент, 
+      // который возращает функция normalizeMove
+      const normalizedMove = normalizeMove(userMove);
+
+      // функция вернет undefined если значение неккоректно
+      // тогда, !undefined === true и выполнится код ниже
+      if (!normalizedMove) {
+        alert('Выберите: камень, ножницы или бумага!');
+        return playRPS();
+      }
+
+      // ход компьютера в RPS
       const computerMove = getComputerMove();
       alert(`Компьютер выбрал: ${computerMove}`);
 
-      const winner = determineWinner(userMove, computerMove);
+      // определение победителя
+      const winner = determineWinner(normalizedMove, computerMove);
       return winner;
     };
 
+    // start марблов
     const start = () => {
+      // обработка продолжения игры
       if (!continueGame) return;
 
+      // сброс счета, чтобы при продолжении начать заново
       userCountBalls = 5;
       computerCountBalls = 5;
 
+      // обработка выхода на этапе игры в RPS
       const rpsWinner = playRPS();
       if (rpsWinner === null) {
         return;
       }
 
+      // обработка ничьей в RPS
       if (rpsWinner === 'ничья') {
         alert('Ничья в игре "Камень, ножницы, бумага". Повторим.');
         return start();
       }
 
+      // выбора первого ходящего в марблах
       if (rpsWinner === 'пользователь') {
         userTurn();
       } else {
@@ -93,6 +128,54 @@
       }
     };
 
+    // обработка счета в марблах
+    // если четность числа игрока совпадает с четностью числа компьютера
+    // то компьютер угадал
+    // иначе компьютер не угадал
+    const updateBalls = (userMove, computerMove, userParity, computerParity) => {
+      if (userParity === computerParity) {
+        alert('Компьютер угадал.');
+        userCountBalls -= userMove;
+        computerCountBalls += userMove;
+      } else {
+        alert('Компьютер не угадал!');
+        userCountBalls += userMove;
+        computerCountBalls -= userMove;
+      }
+
+      console.log('Игрок: ' + userCountBalls);
+      console.log('Компьютер: ' + computerCountBalls);
+      console.log('Чётность юзера: ' + userParity);
+      console.log('Чётность компьютера: ' + computerParity);
+      console.log('Число компьютера: ' + computerMove);
+
+      (computerCountBalls < 0) ? alert('У вас осталось 10 шариков.') : 
+      (userCountBalls < 0) ? alert('У больше не осталось шариков.') : 
+      alert(`У вас осталось: ${userCountBalls} шариков.`);
+    };
+
+    // проверка окончания игры
+    // если у одной и сторон заканчиваются шарики
+    // выводится соответстующее сообщение
+    // затем проверяется, не хочет ли игрок сыграть еще
+    const checkGameOver = () => {
+      if (userCountBalls <= 0) {
+        alert('Поражение');
+      } else if (computerCountBalls <= 0) {
+        alert('У компьютера больше не осталось шариков. Победа');
+      }
+
+      if (userCountBalls <= 0 || computerCountBalls <= 0) {
+        if (confirm('Хотите сыграть ещё?')) {
+          continueGame = true;
+          start();
+        } else {
+          continueGame = false;
+        }
+      }
+    };
+
+    // обработка хода игрока
     const userTurn = () => {
       if (!continueGame) return;
 
@@ -106,57 +189,29 @@
         }
       }
 
-      const userMoveNumber = parseInt(userMove, 10);
+      // обрабатываем неккоректные случаи со стороны хода игрока
+      const userMoveNumber = +userMove;
       if (Number.isNaN(userMoveNumber) || userMoveNumber < 1 || userMoveNumber > 5) {
         alert('Введите число от 1 до 5!');
         return userTurn();
       }
 
+      // генерируем ответ компьютера
       const computerMove = getRandomIntInclusive(1, 5);
       const userParity = (userMoveNumber % 2 === 0) ? 'чёт' : 'нечёт';
       const computerParity = (computerMove % 2 === 0) ? 'чёт' : 'нечёт';
 
-      if (computerParity === userParity) {
-        alert('Компьютер угадал.');
-        userCountBalls -= userMoveNumber;
-        computerCountBalls += userMoveNumber;
-      } else {
-        alert('Компьютер не угадал!');
-        userCountBalls += userMoveNumber;
-        computerCountBalls -= userMoveNumber;
-      }
+      // вызываем обработку счета в марблах и проверяем, не закончилась ли игра
+      updateBalls(userMoveNumber, computerMove, userParity, computerParity);
+      checkGameOver();
 
-      console.log('Игрок: ' + userCountBalls);
-      console.log('Компьютер: ' + computerCountBalls);
-      console.log('Чётность юзера: ' + userParity);
-      console.log('Чётность компьютера: ' + computerParity);
-      console.log('Число компьютера: ' + computerMove);
-
-      (userCountBalls > 10 && computerCountBalls < 0) ? alert('У вас осталось: 10 шариков') :
-          (userCountBalls > 0) ? alert('У вас осталось: ' + userCountBalls + ' шариков.') :
-            alert('У вас больше не осталось шариков...');
-
-      if (userCountBalls <= 0) {
-        alert('Поражение');
-        if (confirm('Хотите сыграть ещё?')) {
-          continueGame = true;
-          start();
-        } else {
-          continueGame = false;
-        }
-      } else if (computerCountBalls <= 0) {
-        alert('У компьютера больше не осталось шариков. Победа');
-        if (confirm('Хотите сыграть ещё?')) {
-          continueGame = true;
-          start();
-        } else {
-          continueGame = false;
-        }
-      } else {
+      // если игра продолжается, то вызываем ход компьютера
+      if (continueGame) {
         computerTurn();
       }
     };
 
+    // обработка хода со стороны компьютера
     const computerTurn = () => {
       if (!continueGame) return;
 
@@ -173,45 +228,22 @@
         }
       }
 
-      const computerQuestionParity = (computerQuestionMove === 'чёт') ? 'чёт' : 'нечёт';
+      // проверка на корректность ввода ответа на вопрос компьютера
+      // normalizeParity вернет undefined, если введено неккоректное значение
+      const normalizedParity = normalizeParity(computerQuestionMove);
 
-      if (computerQuestionParity === computerParity) {
-        alert('Игрок угадал.');
-        computerCountBalls -= computerMove;
-        userCountBalls += computerMove;
-      } else {
-        alert('Игрок не угадал!');
-        computerCountBalls += computerMove;
-        userCountBalls -= computerMove;
+      if (!normalizedParity) {
+        alert('Введите "чёт" или "нечёт"!');
+        return computerTurn();
       }
 
-      console.log('Игрок: ' + userCountBalls);
-      console.log('Компьютер: ' + computerCountBalls);
-      console.log('Чётность юзера: ' + computerQuestionParity);
-      console.log('Чётность компьютера: ' + computerParity);
-      console.log('Число компьютера: ' + computerMove);
+      const computerQuestionParity = normalizedParity;
 
-      (userCountBalls > 10 && computerCountBalls < 0) ? alert('У вас осталось: 10 шариков') :
-          (userCountBalls > 0) ? alert('У вас осталось: ' + userCountBalls + ' шариков.') :
-            alert('У вас больше не осталось шариков...');
+      // вызываем обработку счета в марблах и проверяем, не закончилась ли игра
+      updateBalls(computerMove, computerMove, computerQuestionParity, computerParity);
+      checkGameOver();
 
-      if (userCountBalls <= 0) {
-        alert('Поражение');
-        if (confirm('Хотите сыграть ещё?')) {
-          continueGame = true;
-          start();
-        } else {
-          continueGame = false;
-        }
-      } else if (computerCountBalls <= 0) {
-        alert('У компьютера больше не осталось шариков. Победа');
-        if (confirm('Хотите сыграть ещё?')) {
-          continueGame = true;
-          start();
-        } else {
-          continueGame = false;
-        }
-      } else {
+      if (continueGame) {
         userTurn();
       }
     };
